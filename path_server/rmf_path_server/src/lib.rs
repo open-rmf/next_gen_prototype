db2bddc1a0d3c2b22aeb7541a1e7b348a15c5cdb
Arjo Chakravarty <arjo129@gmail.com>
2026-07-06 15:26:31 +0800

    Add accidentally deleted imports for mapf_post and rclrs
    Signed-off-by: Arjo Chakravarty <arjo129@gmail.com>

--- path_server/rmf_path_server/src/lib.rs
+++ path_server/rmf_path_server/src/lib.rs
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+use mapf_post::{na::Isometry2, MapfResult, SemanticPlan, SemanticWaypoint};
+use rclrs::{IntoPrimitiveOptions, Node};
 use ros_env::{
     builtin_interfaces,
     nav_msgs::msg::{OccupancyGrid, Odometry},
